2359004bbaacd189f9406763029a4ad319e22c1a
jameslove <hello@jameslove.co.uk>
2014-02-03 22:34:04 +0000

    First full commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Remove built directory
+    clean: {
+      build: ['build/']
+    },
+
+    // Built stylesheets with less
+    less: {
+      build: {
+        src: 'assets/less/*',
+        dest: 'build/css/styles.css'
+      }
+    },
+
+    // Build the site using grunt-includes
+    includes: {
+      build: {
+        cwd: 'site',
+        src: [ '*.html', 'pages/*.html' ],
+        dest: 'build/',
+        options: {
+          flatten: true,
+          includePath: 'include',
+          banner: '<!-- Site built using grunt includes! -->\n'
+        }
+      }
+    }
+  });
+
+  // Load plugins used by this task gruntfile
+  grunt.loadNpmTasks('grunt-includes');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-includes');
+
+  // Task definitions
+  grunt.registerTask('build', ['clean', 'less', 'includes']);
+  grunt.registerTask('default', ['build']);
+};
